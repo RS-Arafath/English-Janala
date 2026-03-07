b0008c22e6dab8d206f2e9b9ba3189445547d786
Arafath <rsarafath.signin@gmail.com>
2026-03-08 01:29:06 +0600

    I fixed some issue

--- script/index.js
+++ script/index.js
@@ -19,6 +19,16 @@ const loadLevelWord = (id) => {
 const displayLevelWord = (words) => {
   const wordContainer = document.getElementById('word-container');
   wordContainer.innerHTML = '';
+  if (words.length == 0) {
+    wordContainer.innerHTML = `
+    <div class="text-center   col-span-full space-y-2">
+    <img src="./assets/alert-error.png" alt="alert_img" class="mx-auto">
+        <p class="text-[#79716B] font-light font-bangla text-sm sm:text-base md:text-lg lg:text-xl">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+        <h2 class=" font-semibold text-lg sm:text-xl md:text-2xl lg:text-3xl">নেক্সট Lesson এ যান।</h2>
+      </div>
+    `;
+    return;
+  }
   //get evevry word
   words.forEach((word) => {
     console.log(word);
@@ -29,14 +39,14 @@ const displayLevelWord = (words) => {
         <h2 class="font-bold text-xl md:text-2xl">${word.word}</h2>
         <p class="font-semibold text-base">Meaning /Pronunciation</p>
         <div class="font-bangla text-lg
-         md:text-xl font-medium ">${word.meaning} / ${word.pronunciation}</div>
+         md:text-xl font-medium ">${word.meaning ? word.meaning : 'অর্থ পাওয়া যায় নি'} / ${word.pronunciation}</div>
         <div class="flex justify-between items-center">
           <button class="btn bg-[#ECF6FE] hover:bg-[#aad3f5] duration-300 text-lg py-0 px-3"><i class="fa-solid fa-circle-info"></i></button>
           <button class="btn bg-[#ECF6FE]  hover:bg-[#aad3f5] duration-300 text-lg py-0 px-3"><i class="fa-solid fa-volume-high"></i></button>
         </div>
       </div>
     `;
-    wordContainer.append(card)
+    wordContainer.append(card);
   });
 };
 
