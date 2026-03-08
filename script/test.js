2393004d0791bb70f404a7e771408c5417bc9cb0
Arafath <rsarafath.signin@gmail.com>
2026-03-08 08:24:41 +0600

    I fixed some issue

--- script/test.js
+++
@@ -1,6 +0,0 @@
-const aynonyms = ['hello', 'hi'];
-const createElement = (arr) => {
-  const htmlElement=arr.map(el=>`<span class="btn">${el}</span>`)
-  console.log(htmlElement.join(' '));
-}
-createElement(aynonyms)
